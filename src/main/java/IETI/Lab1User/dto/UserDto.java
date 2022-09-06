7e499cc9b931f8944d6e26841931f9c8afbcb407
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-09-05 22:48:28 -0500

    inicio refactorizacion constructores

--- src/main/java/IETI/Lab1User/dto/UserDto.java
+++ src/main/java/IETI/Lab1User/dto/UserDto.java
@@ -10,25 +10,15 @@ public class UserDto {
     private String createdAt;
     private String password;
 
-    public UserDto(){
-        this.id = String.valueOf((int)(Math.random()*9));
-        this.createdAt = LocalDate.now().toString();
-
-    }
-
-    public UserDto(String name, String email, String lastName, String password){
-        this();
+    public UserDto(String id, String name, String email, String lastName, String createdAt, String password){
+        this.id = id;
         this.name = name;
         this.email = email;
         this.lastName = lastName;
+        this.createdAt = createdAt;
         this.password = password;
     }
 
-    public UserDto(String id,String name, String email, String lastName, String createdAt, String passwords){
-        this(name, email, lastName, passwords);
-        this.id = id;
-        this.createdAt = createdAt;
-    }
 
     public String getId() {
         return id;
